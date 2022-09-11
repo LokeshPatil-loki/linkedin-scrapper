d1417ee19e3f42c7d5fa9d535933e974fb13cd77
LokeshPatil-loki <lokesh1404@outlook.com>
2022-09-12 00:39:53 +0530

    Updated Structure

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
-import ExtractProfileURLs from "./ExtractProfileURLs.js";
-import ScrapeProfiles from "./ScrapeProfile.js";
-import SendConnectionRequest from "./SendConnectionRequest.js"
-import fs from "fs";
+import express from "express";
+import ExtractProfileURLs from "./Automate/ExtractProfileURLs.js";
+import ScrapeProfiles from "./Automate/ScrapeProfile.js";
+import SendConnectionRequest from "./Automate/SendConnectionRequest.js"
 
+const app = express();
+app.use(express.json());
 
-const startTime = new Date().toLocaleTimeString();
-let keywords = "Swaraj Khedekar";
-const profileURLS = await ExtractProfileURLs(keywords);
-const profiles = await ScrapeProfiles(profileURLS);
-console.log(profiles);
+app.post("/extractUrls",async (req,res) => {
+    const keyword = req.body.keyword;
+    const profileUrls = await ExtractProfileURLs(keyword);
+    res.status(200).json(profileUrls);
+});
 
-// SendConnectionRequest(profiles);
+app.post("/getProfiles",async (req,res) => {
+    const profileUrls =req.body.profileUrls;
+    const profiles = await ScrapeProfiles(profileUrls);
+    res.status(200).json(profiles);
+});
 
-const endTime = new Date().toLocaleTimeString();
-console.log("Start Time: " + startTime);
-console.log("End Time: " + endTime);
-fs.writeFileSync("export.json",JSON.stringify(profiles));
+app.post("/invite", async (req,res)=>{
+    const profiles = req.body.profiles;
+    const response = await SendConnectionRequest(profiles,false);
+    res.status(200).json(response);
+});
 
-
-
-
-
-
-
-
-
-
+app.listen(3000,()=>{
+    console.log("Server started at http://localhost:" + 3000 );
+})
